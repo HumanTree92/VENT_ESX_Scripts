ab46123b2c7615ad6770ce08b9f722d01bc7de77
Crizii <135024567+CriziiTV@users.noreply.github.com>
2023-11-25 23:47:47 +0100

    Create de.lua
    German Language File

---
+++ esx_advancedbanking/locales/de.lua
@@ -0,0 +1,13 @@
+Locales['de'] = {
+	['blip_atm'] = 'Geldautomat',
+	['blip_bank'] = 'Bank',
+	['press_access_atm'] = 'Drücken Sie ~INPUT_CONTEXT~ um auf den Geldautomaten zuzugreifen',
+	['press_access_bank'] = 'Drücken Sie ~INPUT_CONTEXT~ um auf die Bank zuzugreifen',
+	['invalid_amount'] = '~r~Das ist ein ungültiger Geldbetrag~s~',
+	['invalid_self'] = '~r~Sie können kein Geld an sich selbst überweisen',
+	['invalid_input'] = '~r~Ungültige Spieler-ID oder Betrag',
+	['transfer_you'] = 'Sie haben ~g~$%s~s~ an ~y~%s~s~ überwiesen',
+	['transfer_receive'] = 'Sie haben ~g~$%s~s~ von ~y~%s~s~ erhalten',
+	['error_vehicle'] = '~r~Sie müssen zu Fuß sein',
+	['error_robbery'] = 'Alle Geldautomaten & Banken sind aufgrund eines kürzlichen Raubüberfalls geschlossen. Kommen Sie später wieder.',
+}
